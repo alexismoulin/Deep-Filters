a6ddab63db1046e6a1e4985f527484c626284e91
Alexis <moulin.alexis@gmail.com>
2021-10-02 22:34:32 -0400

    1 item only

--- Deep Filters/Views/ItemsView.swift
+++ Deep Filters/Views/ItemsView.swift
@@ -4,44 +4,28 @@ import AVKit
 struct ItemsView: View {
     @ObservedObject var appViewModel: AppViewModel
 
-    var body: some View {
-            List(appViewModel.items, id: \.id) { item in
-                ZStack(alignment: .topLeading) {
-                    if item.mediaType == .photo {
-                        Image(uiImage: item.photo ?? UIImage())
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    } else if item.mediaType == .video {
-                        if let url = item.url {
-                            VideoPlayer(player: AVPlayer(url: url))
-                                .frame(minHeight: 200)
-                        } else { EmptyView() }
-                    } else {
-                        if let livePhoto = item.livePhoto {
-                            LivePhotoView(livePhoto: livePhoto)
-                                .frame(minHeight: 200)
-                        } else { EmptyView() }
-                    }
-
-                    Image(systemName: getMediaImageName(using: item))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 24, height: 24)
-                        .padding(4)
-                        .background(Color.black.opacity(0.5))
-                        .foregroundColor(.white)
-            }
-        }
+    var item: PhotoPickerModel? {
+        appViewModel.items.first
     }
 
-    fileprivate func getMediaImageName(using item: PhotoPickerModel) -> String {
-        switch item.mediaType {
-        case .photo:
-            return "photo"
-        case .video:
-            return "video"
-        case .livePhoto:
-            return "livephoto"
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if item?.mediaType == .photo {
+                Image(uiImage: item?.photo ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else if item?.mediaType == .video {
+                if let url = item?.url {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(minHeight: 200)
+                } else { EmptyView() }
+            } else {
+                if let livePhoto = item?.livePhoto {
+                    LivePhotoView(livePhoto: livePhoto)
+                        .frame(minHeight: 200)
+                } else { EmptyView() }
+            }
         }
+
     }
 }
