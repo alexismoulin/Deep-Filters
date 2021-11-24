2e4a0df03b17504b7dbf7218b5faeddba7083ec5
Alexis <moulin.alexis@gmail.com>
2021-11-23 20:26:12 -0500

    removed the tron model

--- Deep Filters/Model/ML Models/StyleEnum.swift
+++ Deep Filters/Model/ML Models/StyleEnum.swift
@@ -13,7 +13,6 @@ enum StyleEnum: String, CaseIterable {
     case style9 = "Style9"
     case style10 = "Style10"
     case style11 = "Style11"
-    case style12 = "Style12"
     case style13 = "Style13"
     case style14 = "Style14"
     case style15 = "Style15"
@@ -23,7 +22,7 @@ enum StyleEnum: String, CaseIterable {
             .style1: "Starry Night", .style2: "June Tree", .style3: "Mosaic",
             .style4: "Pigeons", .style5: "Pixie", .style6: "Aquarium",
             .style7: "Ice", .style8: "Fractal", .style9: "Chromatic",
-            .style10: "Rain Princess", .style11: "Majestic", .style12: "Tron",
+            .style10: "Rain Princess", .style11: "Majestic",
             .style13: "Simpsons", .style14: "Street Art", .style15: "Night Garden"
         ]
         return dict[style, default: "image-missing"]
